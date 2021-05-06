5a23e7af543d3681ae597ee7e6b2ef366bac81d0
Maximiliano Bernasconi <max.bernasconi86@gmail.com>
2021-05-06 14:51:37 -0300

    trabado con is_admin en creacion

---
+++ app/Http/Controllers/UserManagementController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserManagement;
+use Illuminate\Http\Request;
+
+class UserManagementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\UserManagement  $userManagement
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserManagement $userManagement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\UserManagement  $userManagement
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserManagement $userManagement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\UserManagement  $userManagement
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserManagement $userManagement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\UserManagement  $userManagement
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserManagement $userManagement)
+    {
+        //
+    }
+}
